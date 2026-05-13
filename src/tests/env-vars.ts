be6e87c5a77740c73d194119e1eb816fbb012d5d
bjax20 <bdevangelista@up.edu.ph>
2026-05-14 00:02:12 +0800

    fix ci cd

--- src/tests/env-vars.ts
+++ src/tests/env-vars.ts
@@ -1,6 +1,8 @@
-
 process.env.API_PORT = "3000";
 process.env.API_PREFIX = "/api/v1";
 process.env.JWT_SECRET = "test_secret_key_12345";
 process.env.JWT_ISSUER = "TaposTest";
 process.env.NODE_ENV = "test";
+
+process.env.SWAGGER_ENABLE = "1";
+process.env.HEALTH_TOKEN = "test_health_token";
