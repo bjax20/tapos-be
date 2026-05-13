67f8b8cc6f4d6a900db73e09fed0807e3df891c4
bjax20 <bdevangelista@up.edu.ph>
2026-05-13 23:53:50 +0800

    fix test var

---
+++ src/tests/env-vars.ts
@@ -0,0 +1,6 @@
+
+process.env.API_PORT = "3000";
+process.env.API_PREFIX = "/api/v1";
+process.env.JWT_SECRET = "test_secret_key_12345";
+process.env.JWT_ISSUER = "TaposTest";
+process.env.NODE_ENV = "test";
